2d7d3c74a5ad917a09330b3afc6c14d278521b53
한병현 <gksqudgus777@naver.com>
2026-05-25 18:50:55 +0900

    install storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,23 @@
+import type { Preview } from "@storybook/nextjs-vite";
+import { ThemeProvider } from "styled-components";
+import React from "react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ThemeProvider theme={{}}>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default preview;
