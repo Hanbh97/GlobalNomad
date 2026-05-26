3f530542153d83a86587e1262c7cdc066626d6fa
imyoonsoo <bliss38soo@gmail.com>
2026-05-27 05:58:10 +0900

    feat: 쉽게 상태별 아이콘 색상 변경을 위해 kakao.tsx 추가 (assets/icons에)

---
+++ src/assets/icons/kakao.tsx
@@ -0,0 +1,11 @@
+// 상태별 아이콘 색상 변경을 쉽게 하기 위해 아이콘에 kakao.tsx 추가
+const KakaoIcon = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path
+      fill="currentColor"
+      d="M12 3C7.03 3 3 6.14 3 10c0 2.38 1.19 4.47 3 5.74V19l3.45-1.87c.84.23 1.73.37 2.55.37 4.97 0 9-3.14 9-7S16.97 3 12 3z"
+    />
+  </svg>
+);
+
+export default KakaoIcon;
