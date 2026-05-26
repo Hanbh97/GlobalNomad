65411840fac7b11fd3104202cbb8017bd80e7d79
imyoonsoo <bliss38soo@gmail.com>
2026-05-26 22:04:02 +0900

    feat: 공통 — Button 컴포넌트 구현 완료

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -2,15 +2,10 @@ import styled, { css } from "styled-components";
 import ButtonProps from "./type";
 
 const StyledButton = styled.button<ButtonProps>`
-  /* 디폴트 = type: primary, size: lg, state: normal */
+  /* 디폴트 */
   display: flex;
-  width: 342px;
-  height: 54px;
-  background: var(--primary-500);
-  color: var(--color-white);
   font-family: "Pretendard";
-  font-weight: 700;
-  gap: 4px;
+  justify-content: center;
   align-items: center;
 
   /* variant, size에 따른 스타일 */
@@ -25,6 +20,9 @@ const StyledButton = styled.button<ButtonProps>`
       height: 54px;
       border-radius: 16px;
       justify-content: center;
+      font-size: 16px;
+      font-weight: ${variant === "primary" ? 700 : 500};
+      letter-spacing: -0.4px;
     `}
 
   // md
@@ -35,7 +33,9 @@ const StyledButton = styled.button<ButtonProps>`
       gap: ${variant === "text" ? "8px" : "4px"};
       height: 48px;
       border-radius: 14px;
-      justify-content: center;
+      font-size: 16px;
+      font-weight: ${variant === "primary" ? 700 : 500};
+      letter-spacing: -0.4px;
     `}
 
     // sm
@@ -46,17 +46,19 @@ const StyledButton = styled.button<ButtonProps>`
       gap: ${variant === "text" ? "8px" : "4px"};
       height: ${variant === "secondary" ? "34px" : "41px"};
       border-radius: 12px;
+      font-size: 14px;
+      letter-spacing: -0.35px;
     `}
 
     /* padding */
-    // 버튼 타입이 primary일 때
+    // 버튼 타입 primary일 때
     ${({ variant, size }) =>
     variant === "primary" &&
     css`
       padding: ${size === "sm" ? "12px 40px" : "14px 40px"};
     `}
 
-    // 버튼 타입이 secondary일 때 → 3개로 나뉘어져 삼항연산자 사용
+    // 버튼 타입 secondary일 때 → 3개로 나뉘어져 삼항연산자 사용
     ${({ variant, size }) =>
     variant === "secondary" &&
     css`
@@ -64,15 +66,84 @@ const StyledButton = styled.button<ButtonProps>`
         ? "12px 40px"
         : size === "md"
           ? "14px 40px"
-          : "4px 20px;"};
+          : "4px 20px"};
     `} 
 
 
-    // 버튼 타입이 text일 때
+    // 버튼 타입 text일 때
     ${({ variant, size }) =>
     variant === "text" &&
     css`
       padding: ${size === "md" ? "14px 40px 14px 20px" : "12px 40px 12px 20px"};
+    `}
+
+    /* variant */
+    // primary → normal/disabled 상태만 존재
+    ${({ variant }) =>
+    variant === "primary" &&
+    css`
+      background: var(--primary-500);
+      color: var(--color-white);
+      border: none;
+
+      &:disabled {
+        background: var(--gray-200);
+        color: var(--gray-50);
+        cursor: not-allowed;
+      }
+    `}
+
+    // secondary → normal/active/disabled 상태 존재
+    ${({ variant, size }) =>
+    variant === "secondary" &&
+    css`
+      background: var(--color-white);
+      color: var(--gray-600);
+      border: 1px solid var(--gray-200);
+
+      svg {
+        width: ${size === "lg" ? "24px" : size === "md" ? "20px" : "16px"};
+        height: ${size === "lg" ? "24px" : size === "md" ? "20px" : "16px"};
+        flex-shrink: 0;
+        aspect-ratio: 1/1;
+      }
+
+      &:active {
+        background: var(--primary-500);
+        color: var(--color-white);
+        border: none;
+      }
+
+      &:disabled {
+        background: var(--color-white);
+        color: var(--gray-200);
+        border: 1px solid var(--gray-200);
+        cursor: not-allowed;
+      }
+    `}
+    
+    // text → normal/active 상태만 존재
+    ${({ variant, size }) =>
+    variant === "text" &&
+    css`
+      background: none;
+      color: var(--gray-600);
+      border: none;
+
+      svg {
+        width: ${size === "lg" ? "24px" : size === "md" ? "20px" : "16px"};
+        height: ${size === "lg" ? "24px" : size === "md" ? "20px" : "16px"};
+        flex-shrink: 0;
+        fill: var(--gray-600);
+      }
+
+      &:active {
+        background: var(--primary-100);
+        color: var(--gray-950);
+        svg {
+          fill: var(--primary-500);
+        }
+      }
     `}
 `;
 
