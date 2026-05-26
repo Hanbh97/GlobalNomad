1593b73fefd67090ba40311f4018163f333bb7b1
imyoonsoo <bliss38soo@gmail.com>
2026-05-26 15:31:22 +0900

    refactor: 버튼 인터페이스 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,14 @@
+import React from "react";
 import StyledButton from "./style";
 
-const Button = () => {
-  return <StyledButton></StyledButton>;
+export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "text";
+  size?: "lg" | "md" | "sm";
+}
+
+// restProps에 onClick, disable 등 React.HTMLAttributes<HTMLButtonElement>에서 제공하는 props들을 모두 넣기 위해 구조분해할당 처리
+const Button = ({ children, ...restProps }: ButtonProps) => {
+  return <StyledButton {...restProps}>{children}</StyledButton>;
 };
 
 export default Button;
