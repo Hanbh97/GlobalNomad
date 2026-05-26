a33dd13499d7dd2c195e3014f1e4902acf0bbf41
imyoonsoo <bliss38soo@gmail.com>
2026-05-26 22:51:31 +0900

    chore: svg 컴포넌트 사용을 위한 webpack 재설정 및 tsconfig d.ts 포함 추가

--- next.config.ts
+++ next.config.ts
@@ -25,5 +25,46 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: unknown) => {
+      const r = rule as {
+        test?: { test?: (str: string) => boolean };
+        exclude?: unknown;
+      };
+      return r.test?.test?.(".svg");
+    });
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [/url/] },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+              replaceAttrValues: {
+                black: "currentColor",
+                "#000": "currentColor",
+                "#1F1F22": "currentColor",
+                "#707177": "currentColor",
+              },
+            },
+          },
+        ],
+      },
+    );
+    const ruleToExclude = fileLoaderRule as { exclude?: unknown };
+    ruleToExclude.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
+
 export default nextConfig;
