94a23895cf75b4e282637ca8f5df7b16dce9ccd8
eugene <eugenekmpark@gmail.com>
2026-05-27 18:02:18 +0900

    chore: 테스트 코드 삭제

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,12 +1,7 @@
 import Footer from "@/components/layout/Footer";
 
 const Home = () => {
-  return (
-    <>
-      <h1>홈페이지</h1>
-      <Footer />
-    </>
-  );
+  return <h1>홈페이지</h1>;
 };
 
 export default Home;
