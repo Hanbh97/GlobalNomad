cb4dded721b5be30ff65d5fc2f62fb99df3ad9f6
JuHeonParkk <ju10913@naver.com>
2026-05-27 13:42:28 +0900

    chore: main page 테스트 코드 제거

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,5 @@
-import Header from "@/components/layout/Header/Header";
-
 const Home = () => {
-  return (
-    <h1>
-      <Header />
-    </h1>
-  );
+  return <h1>홈페이지</h1>;
 };
 
 export default Home;
