f7aa11b74eb23f9aabfd5c9db22f1d88cc49539b
JuHeonParkk <ju10913@naver.com>
2026-05-27 15:08:54 +0900

    fix: svg 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import type { RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
   compiler: {
@@ -24,6 +25,49 @@ const nextConfig: NextConfig = {
         as: "*.js",
       },
     },
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: RuleSetRule) =>
+        typeof rule !== "string" &&
+        rule.test instanceof RegExp &&
+        rule.test.test(".svg"),
+    ) as RuleSetRule | undefined;
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule?.issuer,
+        resourceQuery: { not: [/url/] },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              icon: true,
+              replaceAttrValues: {
+                black: "currentColor",
+                "#000": "currentColor",
+                "#1F1F22": "currentColor",
+                "#707177": "currentColor",
+              },
+            },
+          },
+        ],
+      },
+    );
+
+    if (!fileLoaderRule) {
+      return config;
+    }
+
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
   },
 };
 export default nextConfig;
