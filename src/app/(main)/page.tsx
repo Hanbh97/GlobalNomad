5e138300594d09c95a0f76e04fed06b10b292449
eugene <eugenekmpark@gmail.com>
2026-05-28 04:30:44 +0900

    feat: 모달 공통 컴포넌트

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,35 @@
+"use client";
+
+import Modal from "@/components/modal";
+import { useState } from "react";
+
 const Home = () => {
-  return <h1>홈페이지</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+
+  return (
+    <>
+      <h1>홈페이지</h1>
+      <button onClick={() => setIsOpen(true)}>sm 모달</button>
+      <br />
+      <button onClick={() => setIsOpen2(true)}>md 모달</button>
+      <br />
+      <button onClick={() => setIsOpen3(true)}>lg 모달</button>
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}></Modal>
+      <Modal
+        isOpen={isOpen2}
+        onClose={() => setIsOpen2(false)}
+        size="md"
+      ></Modal>
+      <Modal
+        isOpen={isOpen3}
+        onClose={() => setIsOpen3(false)}
+        size="lg"
+      ></Modal>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/components/modal.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { createPortal } from "react-dom";
+import React, { useEffect } from "react";
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  size?: "sm" | "md" | "lg";
+  children?: React.ReactNode;
+}
+
+const sizeClass = {
+  sm: "w-[320px] min-h-[140px] rounded-[24px] p-[30px] md:w-[400px] md:min-h-[170px] md:rounded-[30px] md:p-10",
+  md: "w-[320px] min-h-[185px] rounded-[24px] p-[30px] pb-[24px] md:w-[400px] md:min-h-[242px] md:rounded-[30px] md:p-[30px]",
+  lg: "w-[321px] min-h-[493px] rounded-[30px] px-6 py-5 md:w-[385px] md:min-h-[549px] md:py-[30px]",
+};
+
+const Modal = ({ isOpen, onClose, size = "sm", children }: ModalProps) => {
+  useEffect(() => {
+    if (!isOpen) return;
+    const closeOnEsc = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    window.addEventListener("keydown", closeOnEsc);
+    return () => window.removeEventListener("keydown", closeOnEsc);
+  }, [onClose, isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    }
+    return () => {
+      if (isOpen) {
+        document.body.style.overflow = "unset";
+      }
+    };
+  }, [isOpen]);
+
+  if (!isOpen) return null;
+  if (typeof document === "undefined") return null;
+
+  return createPortal(
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+      onMouseDown={onClose}
+    >
+      <div
+        className={`${sizeClass[size]} bg-white`}
+        onMouseDown={(e) => e.stopPropagation()}
+      >
+        {children}
+      </div>
+    </div>,
+    document.body,
+  );
+};
+
+export default Modal;
