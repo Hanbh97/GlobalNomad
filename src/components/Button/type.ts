58562263198e157dae194a4c7f7b1afc7ffdfa2f
imyoonsoo <bliss38soo@gmail.com>
2026-05-28 00:43:38 +0900

    refactor: 버튼 타입명 변경

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,6 +1,6 @@
 import { ButtonHTMLAttributes } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "text";
+  variant?: "onlyText" | "socialIcon" | "userIcon";
   size?: "lg" | "md" | "sm";
 }
