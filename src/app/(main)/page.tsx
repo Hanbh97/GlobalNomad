f65f68a3a5c8f7edd06686cf2c9cd18be3920e00
eugene <eugenekmpark@gmail.com>
2026-05-28 15:10:38 +0900

    chore: 테스트 코드 삭제

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,33 +1,5 @@
-"use client";
-
-import Modal from "@/components/Modal/Modal";
-import { useState } from "react";
-
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isOpen2, setIsOpen2] = useState(false);
-  const [isOpen3, setIsOpen3] = useState(false);
-
-  return (
-    <>
-      <h1>홈페이지</h1>
-      <button onClick={() => setIsOpen(true)}>sm 모달</button>
-      <br />
-      <button onClick={() => setIsOpen2(true)}>md 모달</button>
-      <br />
-      <button onClick={() => setIsOpen3(true)}>lg 모달</button>
-
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        sm 모달
-      </Modal>
-      <Modal isOpen={isOpen2} onClose={() => setIsOpen2(false)} size="md">
-        md 모달
-      </Modal>
-      <Modal isOpen={isOpen3} onClose={() => setIsOpen3(false)} size="lg">
-        lg 모달
-      </Modal>
-    </>
-  );
+  return <h1>홈페이지</h1>;
 };
 
 export default Home;
