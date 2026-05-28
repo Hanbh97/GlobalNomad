f4bcf16447fab1a2b480fd2400d9abb612620a02
JuHeonParkk <ju10913@naver.com>
2026-05-28 16:19:20 +0900

    feat: 프로필 클릭 시 메뉴 드롭다운 및 드롭다운 메뉴 기능 구현

--- src/components/layout/Header/HeaderUserMenu.tsx
+++ src/components/layout/Header/HeaderUserMenu.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import type { User } from "./Header";
+import Dropdown from "@/components/Dropdown/Dropdown";
+
 import BellIcon from "@/assets/icons/bell.svg";
 import DefaultProfileImage from "@/assets/images/default-profile.svg";
 
@@ -10,31 +13,55 @@ type UserProps = {
 };
 
 const HeaderUserMenu = ({ user }: UserProps) => {
-  // Todo: 드롭다운 컴포넌트 생성 후 기능 구현
-  // const [isNoticeOpen, setIsNoticeOpen] = useState(false);
-  // const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const router = useRouter();
+
+  const ProfileMenu = [
+    {
+      label: "마이페이지",
+      onSelect: () => {
+        router.push("/mypage");
+      },
+    },
+    {
+      label: "로그아웃",
+      onSelect: () => {
+        // Todo: 로그아웃 로직 구현
+        alert("로그아웃!");
+      },
+    },
+  ];
 
   return (
     <div className="flex justify-center items-center gap-5">
-      <button className="p-2 rounded-lg text-gray-600 hover:bg-gray-25 hover:text-gray-800 active:scale-95 active:opacity-70 transition">
+      {/* Todo: 알림 기능 구현 */}
+      <button className="p-2 rounded-lg text-gray-600 hover:bg-gray-25 hover:text-primary-500 active:opacity-70 transition">
         <BellIcon width={24} height={24} />
       </button>
       <div className="w-px h-3.5 bg-gray-100" />
-      <button className="flex justify-center items-center gap-2.5 p-2 rounded-lg hover:bg-gray-25 active:scale-95 active:opacity-70 transition">
-        {/* Todo: api 연결 작업 후 수정 */}
-        {user.profileImageUrl ? (
-          <Image
-            src={user.profileImageUrl}
-            alt="프로필 이미지"
-            width={30}
-            height={30}
-            className="w-7.5 h-7.5 rounded-full object-cover"
-          />
-        ) : (
-          <DefaultProfileImage width={30} height={30} />
+      <Dropdown options={ProfileMenu}>
+        {({ toggle }) => (
+          <button
+            onClick={toggle}
+            className="flex justify-center items-center gap-2.5 p-2 rounded-lg hover:bg-gray-25 active:opacity-70 transition"
+          >
+            {/* Todo: api 연결 작업 후 수정 */}
+            {user.profileImageUrl ? (
+              <Image
+                src={user.profileImageUrl}
+                alt="프로필 이미지"
+                width={30}
+                height={30}
+                className="w-7.5 h-7.5 rounded-full object-cover"
+              />
+            ) : (
+              <DefaultProfileImage width={30} height={30} />
+            )}
+            <span className="text-14-medium text-gray-950">
+              {user.nickname}
+            </span>
+          </button>
         )}
-        <span className="text-14-medium text-gray-950">{user.nickname}</span>
-      </button>
+      </Dropdown>
     </div>
   );
 };
