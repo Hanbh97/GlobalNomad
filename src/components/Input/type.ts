a17bb876639b0f831ff818788c6618f59a6d50ab
leeeunji <ayozz6742@naver.com>
2026-05-29 14:40:11 +0900

    리엑트훅폼 적용

--- src/components/Input/type.ts
+++ src/components/Input/type.ts
@@ -7,6 +7,7 @@ export type TextInputProps = {
   type?: "text" | "password";
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
   disabled?: boolean;
   className?: string;
 };
@@ -19,6 +20,7 @@ export type TextAreaProps = {
   errorMessage?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onBlur?: (e: React.FocusEvent<HTMLTextAreaElement>) => void;
   disabled?: boolean;
   className?: string;
   textareaClassName?: string;
