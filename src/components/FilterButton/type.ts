eeaf938f2ef0aaf06938a9020e5bc18a76ba85b3
imyoonsoo <bliss38soo@gmail.com>
2026-05-29 12:00:59 +0900

    refactor: / 사용 안 하도록 수정

--- src/components/FilterButton/type.ts
+++ src/components/FilterButton/type.ts
@@ -1,7 +1,7 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface FilterButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  size?: "pc/tb" | "mo";
+  size?: "pcTb" | "mo";
   icon?: ReactNode;
   isActive?: boolean;
 }
