9016ab73fe57748ae74c9a5427705ccb384f37a3
imyoonsoo <bliss38soo@gmail.com>
2026-05-29 10:37:37 +0900

    refactor: 버튼 type 재사용성 생각해 수정

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,7 +1,8 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "center" | "start";
   size?: "lg" | "md" | "sm";
+  social?: "kakao" | "google";
   icon?: ReactNode;
+  iconPosition?: "start" | "center";
 }
