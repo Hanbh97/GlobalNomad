6e8f5b42fd4941f46ce2bdb5d7a3da706b7cb165
JuHeonParkk <ju10913@naver.com>
2026-05-30 13:19:41 +0900

    fix: header랑 sidemenu z-index 조정

--- src/components/layout/SideMenu/SideMenu.tsx
+++ src/components/layout/SideMenu/SideMenu.tsx
@@ -57,12 +57,12 @@ const SideMenu = () => {
       {isOpen && (
         <div
           onClick={handleCloseSideMenu}
-          className="fixed inset-0 z-40 bg-black/30 backdrop-blur-sm md:hidden"
+          className="fixed inset-0 z-110 bg-black/30 backdrop-blur-sm md:hidden"
         />
       )}
 
       <nav
-        className={`fixed top-[20%] left-0 z-50 w-55 px-3.5 py-6 flex justify-center items-center gap-6 bg-white border border-gray-50 rounded-r-xl shadow-[0_4px_24px_rgba(156,180,202,0.2)] transition-transform duration-300
+        className={`fixed top-[15%] left-0 z-115 md:z-50 w-55 px-3.5 py-6 flex justify-center items-center gap-6 bg-white border border-gray-50 rounded-r-xl shadow-[0_4px_24px_rgba(156,180,202,0.2)] transition-transform duration-300
           ${isOpen ? "translate-x-0" : "-translate-x-[calc(100%-5px)]"}
           lg:w-72.5 md:relative md:translate-x-0 md:rounded-xl`}
       >
@@ -103,7 +103,7 @@ const SideMenu = () => {
           onClick={handleToggleButtonClick}
           aria-label={isOpen ? "메뉴 닫기" : "메뉴 열기"}
           aria-expanded={isOpen}
-          className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-full z-[999] w-8.75 h-40 rounded-r-xl flex items-center justify-center bg-white border border-gray-50 shadow-[0_4px_24px_rgba(156,180,202,0.2)] md:hidden"
+          className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-full z-115 w-8.75 h-40 rounded-r-xl flex items-center justify-center bg-white border border-gray-50 shadow-[0_4px_24px_rgba(156,180,202,0.2)] md:hidden"
         >
           {isOpen ? (
             <ArrowLeft width={24} height={24} />
