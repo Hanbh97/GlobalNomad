e51ab34abf7a83279c9149599f89eb866252c42a
imyoonsoo <bliss38soo@gmail.com>
2026-06-01 06:27:14 +0900

    refactor: 프롭 미전달 시 버튼 라운드 문제로 heightSize 옵셔널 방식 변경

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -14,7 +14,12 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     | "reservationChange"
     | "reservationCancel";
 
-  heightSize?: "lg54" | "md47" | "sm41";
+  /*
+  lg, md, sm 외 모달, 예약내역 카드 등의 라벨 등 여러 곳에서 활용되어 h29, h50 추가
+  * 기존 옵셔널 방식에서 heightSize를 넘기지 않을 시 버튼이 네모로 나오는 문제로
+  * 옵셔널 제거 및 heightSize에 각 사이즈별 rounded 지정
+  */
+  heightSize: "lg54" | "md47" | "sm41" | "h29" | "h50";
   setIcon?: ReactNode;
   iconPosition?: "left" | "center";
 }
