69ae76b09b1de40c5803a9bf0aa0a51d56481dd0
imyoonsoo <bliss38soo@gmail.com>
2026-06-01 06:33:04 +0900

    refactor: 그 외 높이 설정을 className으로 하기 위한 custom heightSize에 추가

--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -18,8 +18,9 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   lg, md, sm 외 모달, 예약내역 카드 등의 라벨 등 여러 곳에서 활용되어 h29, h50 추가
   * 기존 옵셔널 방식에서 heightSize를 넘기지 않을 시 버튼이 네모로 나오는 문제로
   * 옵셔널 제거 및 heightSize에 각 사이즈별 rounded 지정
+  * 
+  * 높이 54, 47, 41, 29, 50을 제외한 나머지 및 여러가지 사이즈에 설정을 className으로 주기 위해 custom 추가
   */
-  heightSize: "lg54" | "md47" | "sm41" | "h29" | "h50";
-  setIcon?: ReactNode;
+  heightSize: "lg54" | "md47" | "sm41" | "h29" | "h50" | "custom"; //
   iconPosition?: "left" | "center";
 }
