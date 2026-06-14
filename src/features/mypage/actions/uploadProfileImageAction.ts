eae8e9e48bac5471fa93481a57b0322f0aabdb61
imyoonsoo <bliss38soo@gmail.com>
2026-06-15 05:26:32 +0900

    feat: 프로필 이미지 생성 server action 구현

---
+++ src/features/mypage/actions/uploadProfileImageAction.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { cookies } from "next/headers";
+import axios from "axios";
+import { MyProfileImageResponse } from "../type";
+
+const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+export const uploadProfileImageAction = async (
+  formData: FormData,
+): Promise<MyProfileImageResponse> => {
+  const cookiesStorage = await cookies();
+  const accessToken = cookiesStorage.get("accessToken")?.value;
+
+  try {
+    const response = await axios.post(
+      `${SERVER_URL}/users/me/image`,
+      formData,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    let errorMessage = "프로필 이미지 업로드에 실패했습니다.";
+    if (axios.isAxiosError(error)) {
+      const statusCode = error.response?.status;
+      if (statusCode === 401) {
+        errorMessage = "로그인 후 이용해 주세요.";
+      } else {
+        errorMessage = error.response?.data?.message ?? errorMessage;
+      }
+    }
+    throw new Error(errorMessage);
+  }
+};
