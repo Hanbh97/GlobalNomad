53db0febf69a608adf3e4b495a10ee0b5980a8ce
imyoonsoo <bliss38soo@gmail.com>
2026-06-15 18:04:06 +0900

    refactor: uploadProfileImage action serverFetchAuth로 리팩토링

--- src/features/mypage/actions/uploadProfileImageAction.ts
+++ src/features/mypage/actions/uploadProfileImageAction.ts
@@ -1,38 +1,18 @@
 "use server";
 
-import { cookies } from "next/headers";
-import axios from "axios";
+import serverFetchAuth from "@/lib/http/serverFetchAuth";
 import { MyProfileImageResponse } from "../type";
-
-const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const uploadProfileImageAction = async (
   formData: FormData,
 ): Promise<MyProfileImageResponse> => {
-  const cookiesStorage = await cookies();
-  const accessToken = cookiesStorage.get("accessToken")?.value;
-
-  try {
-    const response = await axios.post(
-      `${SERVER_URL}/users/me/image`,
-      formData,
-      {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      },
-    );
-    return response.data;
-  } catch (error) {
-    let errorMessage = "프로필 이미지 변경에 실패했습니다.";
-    if (axios.isAxiosError(error)) {
-      const statusCode = error.response?.status;
-      if (statusCode === 401) {
-        errorMessage = "로그인 후 이용해 주세요.";
-      } else {
-        errorMessage = error.response?.data?.message ?? errorMessage;
-      }
-    }
-    throw new Error(errorMessage);
-  }
+  return serverFetchAuth<MyProfileImageResponse>("/users/me/image", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "Content-Type": "", // FormData Content-Type은 자동 설정되도록 지정 X
+    },
+  });
 };
 
 export default uploadProfileImageAction;
