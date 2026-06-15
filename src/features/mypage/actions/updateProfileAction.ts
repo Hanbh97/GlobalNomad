5d817fc3f56f37e343bbc90bc015715d223e7d78
imyoonsoo <bliss38soo@gmail.com>
2026-06-15 17:49:19 +0900

    refactor: getProfile, updateProfile action serverFetchAuth로 리팩토링

--- src/features/mypage/actions/updateProfileAction.ts
+++ src/features/mypage/actions/updateProfileAction.ts
@@ -1,35 +1,15 @@
 "use server";
 
-import { cookies } from "next/headers";
-import axios from "axios";
+import serverFetchAuth from "@/lib/http/serverFetchAuth";
 import { MyProfileRequestBody, MyProfileResponse } from "../type";
-
-const SERVER_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const updateProfileAction = async (
   body: MyProfileRequestBody,
 ): Promise<MyProfileResponse> => {
-  const cookiesStorage = await cookies();
-  const accessToken = cookiesStorage.get("accessToken")?.value;
-
-  try {
-    const response = await axios.patch(`${SERVER_URL}/users/me`, body, {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    });
-    return response.data;
-  } catch (error) {
-    let errorMessage = "내 정보 수정에 실패했습니다.";
-    if (axios.isAxiosError(error)) {
-      const statusCode = error.response?.status;
-      if (statusCode === 400) {
-        errorMessage =
-          error.response?.data?.message ?? "요청 형식이 올바르지 않습니다.";
-      } else if (statusCode === 401) {
-        errorMessage = "로그인 후 이용해 주세요.";
-      }
-    }
-    throw new Error(errorMessage);
-  }
+  return serverFetchAuth<MyProfileResponse>("/users/me", {
+    method: "PATCH",
+    body: JSON.stringify(body),
+  });
 };
 
 export default updateProfileAction;
