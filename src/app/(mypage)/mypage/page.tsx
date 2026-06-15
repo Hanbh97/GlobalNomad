42c8ef5d7e9d4bf63e2bcf59a4be7c1036301468
imyoonsoo <bliss38soo@gmail.com>
2026-06-15 11:14:25 +0900

    refactor: API 연동 이후 page.tsx 임포트 경로 수정

--- src/app/(mypage)/mypage/page.tsx
+++ src/app/(mypage)/mypage/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ProfileEditForm from "@/features/mypage/components/ProfileEditForm/ProfileEditForm";
+import ProfileEditForm from "@/features/mypage/components/ProfileEditForm";
 const MyPage = () => {
   return <ProfileEditForm></ProfileEditForm>;
 };
